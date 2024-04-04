fc6206480851cb65443a37cc3211287e07f1e09f
Cross <99601836+eteryi@users.noreply.github.com>
2024-04-04 16:06:55 +0100

    Added Sounds to use in the Plugins

---
+++ src/main/java/dev/cross/blissfulcore/ui/BSounds.java
@@ -0,0 +1,9 @@
+package dev.cross.blissfulcore.ui;
+
+import org.bukkit.Sound;
+
+public class BSounds {
+    public static final Sound INVENTORY_OPEN = Sound.UI_TOAST_IN;
+    public static final Sound BUTTON_CLICK = Sound.UI_BUTTON_CLICK;
+    public static final Sound INVENTORY_CLOSE = Sound.UI_TOAST_OUT;
+}
